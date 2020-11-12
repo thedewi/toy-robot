42db83d9ae31adc5f9f85f8ea40d1bcc413a4fcf
David Young <thedewi@users.noreply.github.com>
2020-11-12 12:32:55 +1000

    Resolve warnings

--- Robot.Cli/Program.cs
+++ Robot.Cli/Program.cs
@@ -4,7 +4,7 @@ namespace Robot.Cli
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
             Console.WriteLine("Hello World!");
         }
--- Robot.Web/Program.cs
+++ Robot.Web/Program.cs
@@ -10,7 +10,7 @@ namespace Robot.Web
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args)
+        private static IHostBuilder CreateHostBuilder(string[] args)
         {
             return Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
